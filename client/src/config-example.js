7c31cce8f59471b9797d8179ca470ecfff15c23a
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-09-16 17:03:40 -0700

    Add

---
+++ client/src/config-example.js
@@ -0,0 +1,25 @@
+/* Example Configuration 
+ *
+ * Usage:
+ * 1. Rename (or copy) this file to `config.js`. It should reside in `/client/src/`
+ *
+ * 2. Add your desired subreddits to the `subreddits` array below.
+ *
+ *    Only add the name of the subreddit (the part after the `/r/` in the url)
+ *    For example: add `catpics` for 'https://www.reddit.com/r/catpics/'
+ * 
+*/
+const subreddits = [
+  'catpics',
+  'dogpictures',
+  'pics',
+  'streetart',
+  'travelphotos',
+  'earthporn',
+  'MapPorn',
+  'MonumentPorn',
+  'abandonedporn',
+  'vimporn',
+];
+
+export default subreddits;
